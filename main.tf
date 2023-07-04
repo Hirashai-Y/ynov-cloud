6357dbccc1490abbf732a8d9cef38cc65728ab0e
Clement Matray <clement@skyloud.fr>
2023-07-04 16:50:07 +0200

    feat: deploy instance with aws module

--- main.tf
+++ main.tf
@@ -3,3 +3,18 @@ resource "aws_ssm_parameter" "bozo" {
   type  = "String"
   value = "clown"
 }
+
+
+
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "clement-instance"
+  instance_type          = "t2.micro"
+  key_name               = "clement"
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
